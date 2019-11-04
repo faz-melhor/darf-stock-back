524d4aaf07ced8e87615e34ed018e982463c70d4
Lucas Nathan Barbosa de Oliveira <lucas.nathan80@hotmail.com>
2019-11-04 20:01:53 -0300

    Tests with rows lib for pdf scrap and modification on tabula

--- scrapper/xp_scrapper.py
+++ scrapper/xp_scrapper.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import tabula
 
-filename = '../samples/xp-1.pdf'
-multiple_tables = tabula.read_pdf(filename, lattice=True, multiple_tables=True)
-print(multiple_tables[2][5])
+filename = 'samples/xp-2.pdf'
+multiple_tables = tabula.read_pdf(filename, stream=True, guess=False, multiple_tables=True, area=(30,1,50,100), relative_area=True)
+
+print(multiple_tables) 
