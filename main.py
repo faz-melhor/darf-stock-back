dc8a94b9fb24895ea359df2e499dbf6f13c8da88
Lucas Nathan Barbosa de Oliveira <lucas.nathan80@hotmail.com>
2019-12-17 19:53:18 -0300

    Tests with pdf data

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from scrapper.xp_scrapper import XPFlavor
+
 from service.taxes import TaxCalculator
 
 if __name__ == "__main__":
-    file = 'samples/xp-2.pdf'
+    
+    file = 'samples/xp.pdf'
     # print (sys.path)
     xp = XPFlavor()
     # data_matrix = xp.read_pdf(file)
     # xp.read_test()
-    data_matrix =xp.read_test()
+    data_matrix = xp.read_pdf(file)
     lines = data_matrix.apply(xp.create_financial_op, axis=1)
     print(TaxCalculator.calculate_tax(lines))
     #print(lines)
