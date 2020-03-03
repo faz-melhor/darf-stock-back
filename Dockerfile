b68a2023588a02b476dacff4922c1855848983af
Marcos Insfran <rester.marcos@gmail.com>
2020-03-02 21:33:52 -0300

    Update travis to deploy dockerhub

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,19 @@
-FROM python:3.7-slim
+FROM ubuntu:latest
+RUN apt-get update -y
+RUN apt-get install -y python-pip python-dev build-essential
 
 # We copy just the requirements.txt first to leverage Docker cache
 COPY ./requirements.txt /app/requirements.txt
 
 WORKDIR /app
 
-EXPOSE 5000
+COPY . /app
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-COPY . /app
 
 ENTRYPOINT [ "python" ]
 
 CMD [ "app.py" ]
+
--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,9 @@
-# install AWS SDK
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
-
-# install necessary dependency for ecs-deploy
-add-apt-repository ppa:eugenesan/ppa
-apt-get update
-apt-get install jq -y
-
-# install ecs-deploy
-curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
-sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
-
-# login AWS ECR
-eval $(aws ecr get-login --region sa-east-1)
-
-# or login DockerHub
-# docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
-
-# build the docker image and push to an image repository
-docker build -t haoliangyu/ecs-auto-deploy .
-docker tag haoliangyu/ecs-auto-deploy:latest $IMAGE_REPO_URL:latest
-docker push $IMAGE_REPO_URL:latest
-
-# update an AWS ECS service with the new image
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+TAG="latest"
+else
+TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
+docker push $DOCKER_REPO
