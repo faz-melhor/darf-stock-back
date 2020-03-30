8582ec2ce0e3356c9c611a826b2d72b79af454c0
Diogo Augusto <diogo.augustoo@gmail.com>
2020-03-30 20:27:17 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ TAG="latest"
 else
 TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t fazmelhorfoz:$TAG .
-docker tag fazmelhorfoz $DOCKER_REPO
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
